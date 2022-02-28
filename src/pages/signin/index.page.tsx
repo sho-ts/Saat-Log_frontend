02a171d2f48fdab5f5b3f4fa1db9daf40e953a46
tagawa shouta <sho.webfe@gmail.com>
2022-02-28 23:50:36 +0900

    :+1: 仮の認証処理を作成

--- src/pages/signin/index.page.tsx
+++ src/pages/signin/index.page.tsx
@@ -3,20 +3,39 @@ import { FormItem } from '@molecures';
 import { FormContainer } from '@organisms';
 import { withGuestLayout } from '@layouts';
 import { css } from '@emotion/react';
+import { auth } from '@/auth';
+import { useState } from 'react';
 
 const SignIn = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const onClickSignIn = async () => {
+    const res = await auth.signin(email, password);
+    console.log(res);
+  };
+
   return (
     <FormContainer>
       <FormItem heading='メールアドレス'>
-        <TextField type='email' />
+        <TextField
+          value={email}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+          type='email'
+        />
       </FormItem>
       <FormItem heading='パスワード'>
-        <TextField type='password' />
+        <TextField
+          value={password}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+          type='password'
+        />
       </FormItem>
       <Button
         styles={css`
           width: 130px;
         `}
+        onClick={onClickSignIn}
       >
         ログイン
       </Button>
--- src/pages/signup/index.page.tsx
+++ src/pages/signup/index.page.tsx
@@ -3,17 +3,35 @@ import { FormItem } from '@molecures';
 import { FormContainer } from '@organisms';
 import { withGuestLayout } from '@layouts';
 import { css } from '@emotion/react';
+import { auth } from '@/auth';
+import { useState } from 'react';
 
 const SignUp = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const onClickSignUp = async () => {
+    const res = await auth.signup(email, password);
+  };
+
   return (
     <FormContainer>
       <FormItem heading='メールアドレス'>
-        <TextField type='email' />
+        <TextField
+          value={email}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+          type='email'
+        />
       </FormItem>
       <FormItem heading='パスワード'>
-        <TextField type='password' />
+        <TextField
+          value={password}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+          type='password'
+        />
       </FormItem>
       <Button
+        onClick={onClickSignUp}
         styles={css`
           width: 130px;
         `}
