b7dc1c18b8d31adf3ae909cd8d050f333da2bd4b
tagawa shouta <sho.webfe@gmail.com>
2022-03-01 23:09:28 +0900

    :+1: トークン周りのメソッド作成

--- src/auth/Auth.ts
+++ src/auth/Auth.ts
@@ -1,4 +1,5 @@
 import type { Auth0Error } from 'auth0-js';
+import type { NextRouter } from 'next/router';
 import { WebAuth } from 'auth0-js';
 
 class Auth {
@@ -50,6 +51,33 @@ class Auth {
       clientID: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID!,
       returnTo: process.env.NEXT_PUBLIC_AUTH0_LOGOUT_REDIRECT_URL!,
     });
+  }
+
+  getTokenParams(path: string): { [key: string]: string } | null {
+    if (!path.includes('#')) return null;
+
+    return Object.assign(
+      {},
+      ...path
+        .split('#')[1]
+        .split('&')
+        .map((tokenQuery) => {
+          const keyWithValue = tokenQuery.split('=');
+          const tokenObject: { [key: string]: string } = {};
+
+          tokenObject[`${keyWithValue[0]}`] = keyWithValue[1];
+
+          return tokenObject;
+        }),
+    );
+  }
+
+  setToken(tokenName: string, token: string) {
+    localStorage.setItem(tokenName, token);
+  }
+
+  getToken(tokenName: string) {
+    return localStorage.getItem(tokenName);
   }
 }
 
